d72e98f5c2775697d30118c6fbf8327ed2778487
awaisshah228 <awaisshah228@gmail.com>
2021-12-14 01:00:07 +0500

    refresh token mutation implemented

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-const { ApolloServer } = require('apollo-server-express');
-const typeDefs = require('./schema');
-const express = require('express');
-const expressJwt = require('express-jwt');
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./schema");
+const express = require("express");
+const expressJwt = require("express-jwt");
 const cors = require("cors");
-const modelsGraphql = require('../models');
-const _ = require('lodash');
-require('dotenv').config();
-const userResolvers = require('./resolvers/userResolvers');
-const cartResolvers = require('./resolvers/cartResolvers');
-const orderResolvers = require('./resolvers/orderResolvers');
-const productResolvers = require('./resolvers/productResolvers');
-const merchantResolvers = require('./resolvers/merchantResolvers');
-const wishlistResolvers = require('./resolvers/wishlistResolvers');
-const authResolvers = require('./resolvers/authResolvers');
+const cookieParser = require("cookie-parser");
+const modelsGraphql = require("../models");
+const _ = require("lodash");
+require("dotenv").config();
+const userResolvers = require("./resolvers/userResolvers");
+const cartResolvers = require("./resolvers/cartResolvers");
+const orderResolvers = require("./resolvers/orderResolvers");
+const productResolvers = require("./resolvers/productResolvers");
+const merchantResolvers = require("./resolvers/merchantResolvers");
+const wishlistResolvers = require("./resolvers/wishlistResolvers");
+const authResolvers = require("./resolvers/authResolvers");
+const { includes } = require("lodash");
 
 (async function () {
   const server = new ApolloServer({
+    // cors: true,
     cors: true,
     typeDefs,
-    resolvers: _.merge(wishlistResolvers,
+    resolvers: _.merge(
+      wishlistResolvers,
       userResolvers,
       authResolvers,
       cartResolvers,
@@ -31,18 +35,26 @@ const authResolvers = require('./resolvers/authResolvers');
       return {
         models: modelsGraphql,
         user,
-      }
-    }
+        req,
+        res,
+      };
+    },
   });
 
   const app = express();
+  app.set("trust proxy", process.env.NODE_ENV !== "production");
 
   // Middlewares
   app.use(express.json()); // bodyparser
-  app.use(cors());
+
+  app.use(cookieParser());
+  // app.use(cors({
+  //   origin: ["https://studio.apollographql.com"],
+  //   credentials: true
+  // }));
   app.use(
     expressJwt({
-      secret: process.env.JWT_SECRET,
+      secret: process.env.REFRESH_TOKEN_SECRET,
       algorithms: ["HS256"],
       credentialsRequired: false,
     })
@@ -53,16 +65,17 @@ const authResolvers = require('./resolvers/authResolvers');
       return res.status(401).send({ error: "Invalid Request" });
     }
     return next();
-  })
+  });
 
   await server.start();
-  server.applyMiddleware({ app });
+  server.applyMiddleware({
+    app,
+    cors: { origin: "https://studio.apollographql.com", credentials: true },
+  });
 
-  app.use("/api/payment", require('./routes/payment'))
+  app.use("/api/payment", require("./routes/payment"));
 
   app.listen(4000, () =>
-    console.log(
-      `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
-    )
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
   );
 })();
